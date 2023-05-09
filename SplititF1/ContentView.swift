52562508d312d3c8b8d47cb9a4b87e459095856e
Abeer Altalhi <abeerabdrabu17@gmail.com>
2023-05-09 20:10:26 +0300

    Initial Commit

---
+++ SplititF1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SplititF1
+//
+//  Created by Abeer on 19/10/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SplititF1/SplititF1App.swift
@@ -0,0 +1,17 @@
+//
+//  SplititF1App.swift
+//  SplititF1
+//
+//  Created by Abeer on 19/10/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct SplititF1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
