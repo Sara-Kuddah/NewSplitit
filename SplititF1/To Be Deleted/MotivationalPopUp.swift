0706631395e468f34c80cd3cf82c41c9e49c8ca1
AlloTheOne <Allo.busy@gmail.com>
2023-05-27 11:00:32 +0300

    files sorting

--- SplititF1/To Be Deleted/MotivationalPopUp.swift
+++ SplititF1/To Be Deleted/MotivationalPopUp.swift
@@ -1,42 +1,42 @@
+////
+////  MotivationalPopUp.swift
+////  Split it
+////
+////  Created by Dina Alhajj Ibrahim on 17/05/2023.
+////
 //
-//  MotivationalPopUp.swift
-//  Split it
+//import SwiftUI
 //
-//  Created by Dina Alhajj Ibrahim on 17/05/2023.
+//struct MotivationalPopUp: View {
+//    @State private var isAuthenticating = false
+//        @State private var username = ""
+//        @State private var password = ""
 //
-
-import SwiftUI
-
-struct MotivationalPopUp: View {
-    @State private var isAuthenticating = false
-        @State private var username = ""
-        @State private var password = ""
-
-        var body: some View {
-            Button("Press Me") {
-                isAuthenticating.toggle()
-            }
-            .alert("Log in", isPresented: $isAuthenticating) {
-                TextField("Username", text: $username)
-                    .textInputAutocapitalization(.never)
-                SecureField("Password", text: $password)
-                Button("OK", action: authenticate)
-                Button("Cancel", role: .cancel) { }
-            } message: {
-                Text("Please enter your username and password.")
-            }
-        }
-
-        func authenticate() {
-            if username == "twostraws" && password == "sekrit" {
-                print("You're in!")
-            } else {
-                print("Who are you?")
-            }
-        }
-    }
-struct MotivationalPopUp_Previews: PreviewProvider {
-    static var previews: some View {
-        MotivationalPopUp()
-    }
-}
+//        var body: some View {
+//            Button("Press Me") {
+//                isAuthenticating.toggle()
+//            }
+//            .alert("Log in", isPresented: $isAuthenticating) {
+//                TextField("Username", text: $username)
+//                    .textInputAutocapitalization(.never)
+//                SecureField("Password", text: $password)
+//                Button("OK", action: authenticate)
+//                Button("Cancel", role: .cancel) { }
+//            } message: {
+//                Text("Please enter your username and password.")
+//            }
+//        }
+//
+//        func authenticate() {
+//            if username == "twostraws" && password == "sekrit" {
+//                print("You're in!")
+//            } else {
+//                print("Who are you?")
+//            }
+//        }
+//    }
+//struct MotivationalPopUp_Previews: PreviewProvider {
+//    static var previews: some View {
+//        MotivationalPopUp()
+//    }
+//}
